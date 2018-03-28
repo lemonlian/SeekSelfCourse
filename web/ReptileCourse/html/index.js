6bfaf75a4b460c6bea5b034fb221c3abb12182ac
zhengxiaolian <1944670179@qq.com>
2018-03-28 14:02:42 +0800

    change html error

--- web/ReptileCourse/html/index.js
+++ web/ReptileCourse/html/index.js
@@ -9,19 +9,19 @@ $(function () {
            studentId:$("#studentId").val()
             },
             success: function (json) {
-                if (json.code) {
+                if(json.code){
                     alert("请求成功！");
                     let str = '';
-                    str += "<tr><th style='text-align:center;'>教学班</th><th style='text-align:center;'>教学课程</th><th style='text-align:center;'>类别</th><th style='text-align:center;'>教学班分类</th>" +
+                    str += "<tr ><th style='text-align:center;'>教学班</th><th style='text-align:center;'>教学课程</th><th style='text-align:center;'>类别</th><th style='text-align:center;'>教学班分类</th>" +
                         "<th style='text-align:center;'>选课状态</th><th style='text-align:center;'>上课教师</th><th style='text-align:center;'>地点</th><th style='text-align:center;'>上课时间</th>" +
                         "<th style='text-align:center;'>学生名单</th><th style='text-align:center;'>备注</th></tr>";
                     for(var i=0; i < json.data.length; i++) {
                         str += "<tr>";
-                        str += "<td >" + json.data[i].teachingClass + "</td>";
+                        str += "<td >" +json.data[i].teachingClass + "</td>";
                         str += "<td>" + json.data[i].courseIdAndCourseName + "</td>";
                         str += "<td>" +json.data[i].courseCategory + "</td>";
                         str += "<td>" +json.data[i].classClassification + "</td>";
-                        str += "<td >" +json[i].courseSelectionState + "</td>";
+                        str += "<td >" +json.data[i].courseSelectionState + "</td>";
                         str += "<td>" +json.data[i].courseTeacher + "</td>";
                         str += "<td>" +json.data[i].courseAddress + "</td>";
                         str += "<td>" + json.data[i].courseTime + "</td>";
@@ -30,7 +30,8 @@ $(function () {
                         str += "<tr>";
                     }
                     $("#result").append(str);
-                }
+                    str = "";
+            }
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
 
